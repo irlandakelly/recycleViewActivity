3e6951f54986e46d4b28741ed98026ebab80de67
unknown <irlandakelly3@gmail.com>
2022-10-26 08:01:06 -0600

    created project

---
+++ app/src/main/java/com/example/recyclerview261022/CustomAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.recyclerview261022
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
