8900a0f639302d921e448a25bc5619391034cc9c
unknown <irlandakelly3@gmail.com>
2022-10-28 07:54:03 -0600

    added custom adapter class

--- app/src/main/java/com/example/recyclerview261022/CustomAdapter.kt
+++ app/src/main/java/com/example/recyclerview261022/CustomAdapter.kt
@@ -1,20 +1,45 @@
 package com.example.recyclerview261022
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+    val titles = arrayOf("Netflix", "Suscríbete", "Videos", "YouTube")
+    val details = arrayOf("Muchos videos", "Más videos", "Gran cantidad", "Kotlin")
+    val images = intArrayOf(R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).
+        inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemDetail.text = details[i]
+        viewHolder.itemImage.setImageResource(images[i])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return titles.size;
     }
 }
--- app/src/main/java/com/example/recyclerview261022/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview261022/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.recyclerview261022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adaptador = CustomAdapter();
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adaptador
     }
 }
